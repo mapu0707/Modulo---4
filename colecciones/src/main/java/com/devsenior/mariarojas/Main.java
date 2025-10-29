179c193641e5a79ed028edddf5c59db09fbbbfb2
Maria Paula Rojas <mariaprojas951@gmail.com>
2025-10-28 19:49:03 -0500

    first commit

---
+++ colecciones/src/main/java/com/devsenior/mariarojas/Main.java
@@ -0,0 +1,12 @@
+package com.devsenior.mariarojas;
+
+public class Main {
+    public static void main(String[] args) {
+       metodo1(); 
+    }
+
+    public static void metodo1(){
+        int[] ids ={12,34,45,67,89};
+         
+    }
+}
